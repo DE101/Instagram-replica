9117094144e9cc60eb08fd13347f3f1c77014472
DE101 <dinganaearnestfohtung@gmail.com>
2023-08-09 08:28:55 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="side-bar"></div>
+      <div className="main-content"></div>
+      <div className="right-side-bar"></div>
     </div>
   );
 }
