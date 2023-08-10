a1362d9970cfa269d7b5adfb72793747e18854ee
DE101 <dinganaearnestfohtung@gmail.com>
2023-08-10 21:54:13 +0100

    created menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {logo} from './logo'
 function App() {
   return (
     <div className="container">
-      <div className="side-bar"></div>
+      <div className="side-bar">{logo}</div>
       <div className="main-content"></div>
       <div className="right-side-bar"></div>
     </div>
