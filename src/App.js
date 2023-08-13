8a500cd75569891ab9e0f34905c3105dbfeb0e85
DE101 <dinganaearnestfohtung@gmail.com>
2023-08-13 22:32:19 +0100

    updated app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ function App() {
       <div className="side-bar">
         <Logo></Logo>
         <div>
-          <Menu></Menu>
+          <Menu text="Home" icon="home"></Menu>
+          <Menu text="Search" icon="search"></Menu>
+          <Menu text="Explore" icon="explore"></Menu>
+          <Menu text="Reels" icon="movie"></Menu>
+          <Menu text="Messages" icon="message"></Menu>
+          <Menu text="Notifications" icon="favorite"></Menu>
+          <Menu text="Create" icon="add_box"></Menu>
+          <Menu text="Profile" icon="account_circle"></Menu>
         </div>
       </div>
       <div className="main-content"></div>
