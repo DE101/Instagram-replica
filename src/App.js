25baa8629916bba092457ab5921a573548a7a9e7
DE101 <dinganaearnestfohtung@gmail.com>
2024-02-06 22:30:22 +0100

    changed content

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
-import './App.css';
-import Logo from './LogoE';
-import Menu from './Menu';
-import Post from './Post';
-import FollowBack from './FollowBack';
-import { Route,Routes } from 'react-router-dom';
+import "./App.css";
+import Logo from "./LogoE";
+import Menu from "./Menu";
+import Post from "./Post";
+import FollowBack from "./FollowBack";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const followersList = [
     {
       name: "Kaiser",
-      avatar: "https://orig00.deviantart.net/8384/f/2007/302/0/9/random_head_profile_by_kurvos.png"
+      avatar:
+        "https://orig00.deviantart.net/8384/f/2007/302/0/9/random_head_profile_by_kurvos.png",
     },
     {
       name: "Sheeran",
-      avatar: "https://i.imagesup.co/images2/42736a080ed7a88afcf537cb6309cbe3d037e878.jpg"
+      avatar:
+        "https://i.imagesup.co/images2/42736a080ed7a88afcf537cb6309cbe3d037e878.jpg",
     },
     {
       name: "Kaiser",
-      avatar: "https://orig00.deviantart.net/8384/f/2007/302/0/9/random_head_profile_by_kurvos.png"
+      avatar:
+        "https://orig00.deviantart.net/8384/f/2007/302/0/9/random_head_profile_by_kurvos.png",
     },
     {
       name: "Sheeran",
-      avatar: "https://i.imagesup.co/images2/42736a080ed7a88afcf537cb6309cbe3d037e878.jpg"
-    }
-  ]
+      avatar:
+        "https://i.imagesup.co/images2/42736a080ed7a88afcf537cb6309cbe3d037e878.jpg",
+    },
+  ];
   const Posts = [
     {
-      postMediaLink: "https://th.bing.com/th/id/R.8486fc5ea4e6586190a49a4d91170ea8?rik=kISlrokwfE3CRg&pid=ImgRaw&r=0",
+      postMediaLink:
+        "https://th.bing.com/th/id/R.8486fc5ea4e6586190a49a4d91170ea8?rik=kISlrokwfE3CRg&pid=ImgRaw&r=0",
       postMeta: "Ice cream joy",
-      avatar: "https://th.bing.com/th/id/OIP.onvo20gh0-YyVWDiLcICowHaHa?pid=ImgDet&w=512&h=512&rs=1",
-      name:"John Doe",
-      bio:"🥹 I am a fun teacher!"
-    }
-  ]
+      avatar:
+        "https://th.bing.com/th/id/OIP.onvo20gh0-YyVWDiLcICowHaHa?pid=ImgDet&w=512&h=512&rs=1",
+      name: "John Doe",
+      bio: "🥹 I am a fun teacher!",
+    },
+  ];
   return (
     <div className="container">
       <div className="side-bar">
@@ -55,27 +61,24 @@ function App() {
         </div>
       </div>
       <div className="main-content">
-        {
-          Posts.map((post, i)=>{
-            return (
-              <Post post={post} key={i}>
-              </Post>
-            )
-          })
-        }
+        <Routes>
+          <Route path="/Posts" Component={Post}></Route>
+          <Route></Route>
+        </Routes>
+        {Posts.map((post, i) => {
+          return <Post post={post} key={i}></Post>;
+        })}
       </div>
       <div className="right-side-bar">
-        {
-          followersList.map((follower, i )=>{
-            return (
-              <FollowBack
-                avatar={follower.avatar}
-                name={follower.name}
-                key={i}
-                ></FollowBack>
-            )
-          })
-        }
+        {followersList.map((follower, i) => {
+          return (
+            <FollowBack
+              avatar={follower.avatar}
+              name={follower.name}
+              key={i}
+            ></FollowBack>
+          );
+        })}
         {/* <FollowBack avatar="https://orig00.deviantart.net/8384/f/2007/302/0/9/random_head_profile_by_kurvos.png" name="Kaiser"></FollowBack>
         <FollowBack avatar="https://i.imagesup.co/images2/42736a080ed7a88afcf537cb6309cbe3d037e878.jpg" name="Sheeran"></FollowBack> */}
       </div>
